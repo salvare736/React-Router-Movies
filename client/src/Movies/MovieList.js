e29cf9994fa08ef781bcb86a83d8f5fcc676d76f
Sergio Alvarez <salvare736@gmail.com>
2021-03-22 17:50:27 -0400

    achieved some base level of functionality; still need to refine everything and make the 'home' link work

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 export default function MovieList(props) {
+  const { movies } = props;
+
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {movies.map(movie => (
+      <Link key={movie.id} to={`/movies/${movie.id}`}>
+        <MovieDetails movie={movie} />
+      </Link>
       ))}
     </div>
   );
